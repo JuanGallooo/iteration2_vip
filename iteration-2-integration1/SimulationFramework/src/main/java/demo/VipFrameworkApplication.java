eca90a2168351287546722cb92bff4e40605771c
sas1 <sas1@grid2.icesi.edu.co>
2020-07-22 10:16:46 -0500

    fix springcontroller error

--- iteration-2-integration1/SimulationFramework/src/main/java/demo/VipFrameworkApplication.java
+++ iteration-2-integration1/SimulationFramework/src/main/java/demo/VipFrameworkApplication.java
@@ -12,11 +12,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import DataSource.Persistence.PlanVersionRepository;
 import SystemState.SITMFactory.SITMPlanVersion;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import DataSource.Persistence.PlanVersionRepository;
+import SystemState.SITMFactory.SITMPlanVersion;
+
 @SpringBootApplication
-@ComponentScan("DataSource")
+@ComponentScan("com.simulationFramework.DataSource")
 @EnableJpaRepositories
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class VipFrameworkApplication {
+	
 	
 	private PlanVersionRepository plansVersionRepository;
 	
